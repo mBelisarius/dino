2446cb3ce9ceb2381f747293fb845be92ac14f64
AndreLuisPLuz <andreluisplzu@gmail.com>
2023-10-25 09:57:15 -0300

    Game perceptions added

--- src/events/events.c
+++ src/events/events.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <stdbool.h>
+#include "../worldgen/world_gen.h"
 
-int main(void)
+bool crouched = false;
+
+void perceive(Object* dino, int** game_matrix, int command)
 {
-    char ch;
-    printf("Press 'q' to exit prom program\n");
-    do{
-        ch = _getch();
-        printf("%c (%d)\n", ch, ch);
-    } while( ch != 'q');
+    short dinosaur_parts = 0; // keeps track of how many dinosaur parts are visible
+
+    if (crouched)
+    {
+        int curr_row = dino->y;
+        for (int i = 0; i < X; i++)
+        {
+            dinosaur_parts += (game_matrix[i][curr_row] == Dino) ? 1 : 0;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < Y; i++)
+        {
+            dinosaur_parts += (game_matrix[4][i] == Dino) ? 1 : 0;
+        }
+    }
+
+    if (dinosaur_parts != 2)
+    {
+        // morreu
+    }
+
+    switch (command)
+    {
+        case 119:
+            // função de pular
+            break;
+
+        case 115:
+            // função de abaixar
+            break;
+        
+        default:
+            // faz nada
+            break;
+    }
 }
