6c0786eaa857029d8a4d192538f880df7efbc717
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2023-10-25 10:59:58 -0300

    imagens todas prontas

--- Visual/imagem.c
+++ Visual/imagem.c
@@ -1,66 +1,15 @@
-// #define STB_IMAGE_IMPLEMENTATION
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include "stb_image.h"
-
-// int main() {
-//     const char* filename = "../pixil-layer-Background.png";
-//     int width, height, channels;
-//     unsigned char* image = stbi_load(filename, &width, &height, &channels, STBI_rgb);
-//     int dino[20][20];
-
-//     if (image != NULL) {
-//         int threshold = 128; // Limiar (ajuste conforme necessário)
-
-//         for (int y = 0; y < height; y++) {
-//             for (int x = 0; x < width; x++) {
-//                 int index = (x + y * width) * 3; // 3 canais (STBI_rgb)
-//                 unsigned char r = image[index];
-//                 unsigned char g = image[index + 1];
-//                 unsigned char b = image[index + 2];
-//                 unsigned char pixel = (r + g + b) / 3; // Converter para escala de cinza
-
-//                 if (pixel < threshold) {
-//                     dino[x][y] = 9; // Se o RGB do pixel for igual a da cor BRANCA armazena 1 na matriz
-//                                     // Valor abaixo do limiar
-//                 } else {
-//                     dino[x][y] = 1; // Se o RGB do pixel for igual a da cor PRETA armazena 0 na matriz
-//                                     // Valor acima do limiar
-//                 }
-//             }
-            
-//         }
-
-//         // Libere a memória alocada para a imagem
-//         stbi_image_free(image);
-//     } else {
-//         printf("Erro ao carregar a imagem.\n");
-//     }
-
-
-
-//     //print matriz dino
-//     for (int y = 0; y < height; y++) 
-//     {
-//         for (int x = 0; x < width; x++) 
-//         {
-//             printf("%i ", dino[x][y]);
-//         }
-//         printf("\n");
-//     }
-    
-//     return 0;
-// }
 #define STB_IMAGE_IMPLEMENTATION
 #include <stdio.h>
 #include <stdlib.h>
 #include "stb_image.h"
 
+//DINO1
+
 int main() {
-    const char* filename = "obstaculo-predio-pequeno.png";
+    const char* filename = "dino1.png";
     int width, height, channels;
     unsigned char* image = stbi_load(filename, &width, &height, &channels, STBI_rgb);
-    int dino[45][45];
+    int dino1[8][16];
 
     if (image != NULL) {
         int threshold = 128; // Limiar (ajuste conforme necessário)
@@ -74,10 +23,10 @@ int main() {
                 unsigned char pixel = (r + g + b) / 3; // Converter para escala de cinza
 
                 if (pixel < threshold) {
-                    dino[x][y] = 0; // Se o RGB do pixel for igual a da cor BRANCA armazena 1 na matriz
+                    dino1[x][y] = 0; // Se o RGB do pixel for igual a da cor BRANCA armazena 1 na matriz
                                     // Valor abaixo do limiar
                 } else {
-                    dino[x][y] = 1; // Se o RGB do pixel for igual a da cor PRETA armazena 0 na matriz
+                    dino1[x][y] = 1; // Se o RGB do pixel for igual a da cor PRETA armazena 0 na matriz
                                     // Valor acima do limiar
                 }
             }
@@ -93,21 +42,25 @@ int main() {
 
 
     //print matriz dino
-    for (int y = 0; y < height; y++) 
-    {
-        for (int x = 0; x < width; x++) 
+
+        for (int y = 0; y < height; y++) 
         {
-            if (dino[x][y] == 1)
+            for (int x = 0; x < width; x++) 
             {
-                printf("  ");
-            }
-            else{
-                printf("%i ", dino[x][y]);
-            }
+                if (dino1[x][y] == 1)
+                {
+                    printf("  ");
+                }
+                else
+                {
+                    printf("%i ", dino1[x][y]);
+                }
             
+            }
+            printf("\n");
         }
-        printf("\n");
-    }
+
+
     
     return 0;
 }
