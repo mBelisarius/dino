71f00057e2ec33c0a6783de27573ea73ce88274b
AndreLuisPLuz <andreluispluz@gmail.com>
2023-10-26 08:51:08 -0300

    Create events.h

---
+++ src/events/events.h
@@ -0,0 +1,11 @@
+#ifndef EVENTS_H
+#define EVENTS_H
+
+    #include "../worldgen/world_gen.h"
+
+    void perceive(Object* dino, int** game_matrix, int command);
+    int get_input();
+    void fill_movements(int* movements);
+    void jump(Object* dino, int* movements, unsigned* length);
+
+#endif
