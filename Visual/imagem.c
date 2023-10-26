0cbe71afb92ac54afe88b6e5249440669624493a
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2023-10-26 10:24:15 -0300

    Add header with frames

--- Visual/imagem.c
+++ Visual/imagem.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "stb_image.h"
+#include "frames.h"
 
 //DINO1
-
-int main() {
-    const char* filename = "dino1.png";
+int gerarMatrizImagem(char *imagem, char file[]){
+    const char* filename = imagem;
     int width, height, channels;
     unsigned char* image = stbi_load(filename, &width, &height, &channels, STBI_rgb);
-    int dino1[8][16];
+    int matriz_imagem[8][24];
+
+    FILE * arq = fopen( file, "w");
 
     if (image != NULL) {
         int threshold = 128; // Limiar (ajuste conforme necessário)
@@ -23,44 +25,46 @@ int main() {
                 unsigned char pixel = (r + g + b) / 3; // Converter para escala de cinza
 
                 if (pixel < threshold) {
-                    dino1[x][y] = 0; // Se o RGB do pixel for igual a da cor BRANCA armazena 1 na matriz
+                    matriz_imagem[x][y] = 0; // Se o RGB do pixel for igual a da cor BRANCA armazena 1 na matriz
                                     // Valor abaixo do limiar
                 } else {
-                    dino1[x][y] = 1; // Se o RGB do pixel for igual a da cor PRETA armazena 0 na matriz
+                    matriz_imagem[x][y] = 1; // Se o RGB do pixel for igual a da cor PRETA armazena 0 na matriz
                                     // Valor acima do limiar
                 }
             }
             
         }
 
+         for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if(matriz_imagem[x][y] == 0) {
+                    fprintf(arq,"#");
+                }
+                else{
+                    fprintf(arq,".");
+                }
+            }
+            fprintf(arq,"\n");
+        }
+            
         // Libere a memória alocada para a imagem
         stbi_image_free(image);
     } else {
         printf("Erro ao carregar a imagem.\n");
     }
+}
 
 
-
-    //print matriz dino
-
-        for (int y = 0; y < height; y++) 
-        {
-            for (int x = 0; x < width; x++) 
-            {
-                if (dino1[x][y] == 1)
-                {
-                    printf("  ");
-                }
-                else
-                {
-                    printf("%i ", dino1[x][y]);
-                }
-            
-            }
-            printf("\n");
-        }
-
-
+int main() {
     
+    int dino1 = gerarMatrizImagem("obstaculo-predio-grande.png", "obstacles_big_building.txt");
+
+    printf("%s", dino_run_1);
+    printf("%s", dino_run_2);
+    printf("%s", dino_crouch);
+    printf("%s", obstacles_big_building);
+    
+    
+  
     return 0;
 }
